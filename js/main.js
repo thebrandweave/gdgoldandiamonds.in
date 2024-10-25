95f4786731c24f920744d66e63b74097faaca94f
Thameem-20 <ahmedthameem20@gmail.com>
2024-10-25 21:47:36 +0530

    updated

--- js/main.js
+++
@@ -1,5 +0,0 @@
-// Loader
-window.addEventListener('load', function() {
-    const loader = document.querySelector('.loading');
-    loader.style.display = "none"; // Corrected 'Style' to 'style'
-});
