d054ea7f6ed481e6aeabe2104c2d0568fd59ec16
Munazza Begam <118593452+munazzabegam@users.noreply.github.com>
2025-07-25 10:33:57 +0530

    Update config.php

--- adminFiles/config.php
+++ adminFiles/config.php
@@ -3,7 +3,7 @@ $host = $_SERVER['HTTP_HOST'];
 
 if (strpos($host, 'gdgoldanddiamonds.in') !== false) {
     // If the URL contains 'gdgoldanddiamonds.in', use this connection
-    $conn = new mysqli("localhost", "u593219986_root", "GdGold&Diamonds1234", "u593219986_gd_gold");
+    $conn = new mysqli("localhost", "u232955123_brandweave24", "Brandweave@24", "u232955123_liyasdiamond");
     // echo "this is in server";
 
 } else {
