9192a59b21634459f6fbce22d3cf87166aae8fdb
ashiqaa21 <ashikahaneef62@gmail.com>
2026-06-27 11:13:40 +0530

    updated ui popup and admin files

--- contact.php
+++ contact.php
@@ -12,11 +12,12 @@ include("./adminFiles/config.php");
   <title>Contact Us - Liyas Gold and Diamonds</title>
 
   <link rel="icon" type="image/x-icon" href="./images/favicon.ico">
-  <link rel="stylesheet" href="./css/style.css?v=1.2" />
-  <link rel="stylesheet" href="./css/navBar.css?v=1.2" />
-  <link rel="stylesheet" href="./css/footer.css?v=1.2" />
-  <link rel="stylesheet" href="./css/testimonials.css?v=1.2" />
-  <link rel="stylesheet" href="./css/responsive/phone.css?v=1.2">
+  <link rel="stylesheet" href="./css/style.css?v=<?php echo filemtime('./css/style.css'); ?>" />
+  <link rel="stylesheet" href="./css/navBar.css?v=<?php echo filemtime('./css/navBar.css'); ?>" />
+  <link rel="stylesheet" href="./css/footer.css?v=<?php echo filemtime('./css/footer.css'); ?>" />
+  <link rel="stylesheet" href="./css/testimonials.css?v=<?php echo filemtime('./css/testimonials.css'); ?>" />
+  <link rel="stylesheet" href="./css/responsive/phone.css?v=<?php echo filemtime('./css/responsive/phone.css'); ?>">
+  <link rel="stylesheet" href="./css/popup.css?v=<?php echo filemtime('./css/popup.css'); ?>">
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
   <!-- Font Awesome -->
